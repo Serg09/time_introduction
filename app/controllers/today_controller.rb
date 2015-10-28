f3f6e0d0b33935c14758e6af0e860d92c7ff6f61
Serg09 <sergeyskumatov@aol.com>
2015-10-27 20:54:23 -0600

    Modified controller and view
    [#106277080]

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -1,4 +1,9 @@
 class TodayController < ApplicationController
-  def time
+  def now
+    @now = params[:now]
+    case params[:now]
+      when 'now'
+        @now = "The current time is #{Time.current.strftime("%a, %d %b %Y %I :%M :%S %Z %:z ")}"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'time' => 'today#time'
-  get 'time/:time_now' => 'today#time'
+  # get 'now' => 'today#now'
+  get 'time/:now' => 'today#now'
   get 'introduce' => 'introduce#introduce'
   # get 'introduce/:name_one' => 'introduce#introduce'
   get 'introduce/:name_one/and/:name_two' => 'introduce#introduce'
